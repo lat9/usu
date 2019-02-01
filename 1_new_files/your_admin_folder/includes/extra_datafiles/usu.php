aabe5271cbb233a92c4ffd5cffb971c4edd4390d
pRose_ <prose@rossroberts.com>
2019-02-01 07:50:50 -0800

    fixes undefined function get-info-page on admin

--- 1_new_files/your_admin_folder/includes/extra_datafiles/usu.php
+++ 1_new_files/your_admin_folder/includes/extra_datafiles/usu.php
@@ -7,3 +7,26 @@
  */
 
 define('TABLE_USU_CACHE', DB_PREFIX . 'usu_cache');
+
+if (function_exists('zen_get_info_page')) {
+    return;
+} else {
+
+    if (defined('IS_ADMIN_FLAG') && (IS_ADMIN_FLAG == 1)) {
+
+        function zen_get_info_page($zf_product_id)
+        {
+            global $db;
+            $sql = "select products_type from " . TABLE_PRODUCTS . " where products_id = '" . (int)$zf_product_id . "'";
+            $zp_type = $db->Execute($sql);
+            if ($zp_type->RecordCount() == 0) {
+                return 'product_info';
+            } else {
+                $zp_product_type = $zp_type->fields['products_type'];
+                $sql = "select type_handler from " . TABLE_PRODUCT_TYPES . " where type_id = '" . (int)$zp_product_type . "'";
+                $zp_handler = $db->Execute($sql);
+                return $zp_handler->fields['type_handler'] . '_info';
+            }
+        }
+    }
+}
