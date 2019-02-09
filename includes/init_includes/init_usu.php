cc29d82e783f1d83fdb8edfad349ee28944058bd
lat9 <lat9@vinosdefrutastropicales.com>
2019-02-09 09:40:51 -0500

    #11: Refactor prior to changes
    ... changing tabs to 4-spaces, formatting conditionals and
    class/function braces.

--- includes/init_includes/init_usu.php
+++ includes/init_includes/init_usu.php
@@ -2,32 +2,35 @@
 /**
  * Part of Ultimate URLs for Zen Cart.
  *
+ * @copyright Copyright 2019        Cindy Merkin (vinosdefrutastropicales.com)
  * @copyright Copyright 2012 - 2015 Andrew Ballanger
  * @license http://www.gnu.org/licenses/gpl.txt GNU GPL V3.0
  */
 if (!defined('IS_ADMIN_FLAG')) {
-	die('Illegal Access');
+    die('Illegal Access');
 }
 
 // Remove the configured extension if present
-if(isset($_GET['main_page'])) {
-	if(defined('USU_END') && USU_END != '') {
-		$pos = strrpos($_GET['main_page'], USU_END);
-		if($pos !== false) {
-			$_GET['main_page'] = substr($_GET['main_page'], 0, $pos);
-		}
-	}
+if (isset($_GET['main_page'])) {
+    if (defined('USU_END') && USU_END != '') {
+        $pos = strrpos($_GET['main_page'], USU_END);
+        if ($pos !== false) {
+            $_GET['main_page'] = substr($_GET['main_page'], 0, $pos);
+        }
+    }
 
-	if($_GET['main_page'] == FILENAME_PRODUCT_INFO) {
-		// Retrieve the product type handler from the database
-		$type = $db->Execute(
-			'SELECT `pt`.`type_handler` ' .
-			'FROM `'. TABLE_PRODUCTS .'` AS `p` ' .
-			'LEFT JOIN `'. TABLE_PRODUCT_TYPES .'` AS `pt` ON `pt`.`type_id` = `p`.`products_type` ' .
-			'WHERE `p`. `products_id` = \'' . (int)$_GET['products_id'] . '\' LIMIT 1'
-		);
-		if(!$type->EOF) {
-			$_GET['main_page'] = $type->fields['type_handler'] . '_info';
-		}
-	}
+    if ($_GET['main_page'] == FILENAME_PRODUCT_INFO && isset($_GET['products_id'])) {
+        // Retrieve the product type handler from the database
+        $type = $db->Execute(
+            "SELECT pt.type_handler
+               FROM " . TABLE_PRODUCTS . " p
+                    LEFT JOIN " . TABLE_PRODUCT_TYPES . "pt
+                        ON pt.type_id = p.products_type
+              WHERE p.products_id = " . (int)$_GET['products_id'] . "
+              LIMIT 1"
+        );
+        if (!$type->EOF) {
+            $_GET['main_page'] = $type->fields['type_handler'] . '_info';
+        }
+    }
 }
