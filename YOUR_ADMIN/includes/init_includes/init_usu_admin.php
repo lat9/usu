4017a406a34e3f74a7cdb334ed1241565b237368
lat9 <cindy@vinosdefrutastropicales.com>
2021-01-01 13:23:52 -0500

    Update readme/init for v3.0.7 release

--- YOUR_ADMIN/includes/init_includes/init_usu_admin.php
+++ YOUR_ADMIN/includes/init_includes/init_usu_admin.php
@@ -2,7 +2,7 @@
 /**
  * Part of Ultimate URLs, v3.0.0+, for Zen Cart.
  *
- * @copyright Copyright 2019-2020 Cindy Merkin (vinosdefrutastropicales.com)
+ * @copyright Copyright 2019-2021 Cindy Merkin (vinosdefrutastropicales.com)
  * @license http://www.gnu.org/licenses/gpl.txt GNU GPL V3.0
  */
 if (!defined('IS_ADMIN_FLAG') || IS_ADMIN_FLAG !== true) {
@@ -14,8 +14,8 @@ if (!defined('IS_ADMIN_FLAG') || IS_ADMIN_FLAG !== true) {
 // last_modified date for the USU_VERSION configuration setting is updated to reflect
 // the current update-date.
 //
-define('USU_CURRENT_VERSION', '3.0.7-beta1');
-define('USU_CURRENT_UPDATE_DATE', '2020-12-24');
+define('USU_CURRENT_VERSION', '3.0.7');
+define('USU_CURRENT_UPDATE_DATE', '2021-01-01');
 
 // -----
 // Wait until an admin is logged in before seeing if any initialization steps need to be performed.
