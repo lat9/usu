499df834a7f2aa01a29629a0c66bcfbc3decb31d
lat9 <cindy@vinosdefrutastropicales.com>
2022-03-10 12:01:38 -0500

    #48: Update to remove `zenAdminID` parameter from admin-generated catalog hrefs.

--- YOUR_ADMIN/includes/classes/observers/UsuAdminObserver.php
+++ YOUR_ADMIN/includes/classes/observers/UsuAdminObserver.php
@@ -2,30 +2,30 @@
 /**
  * Part of Ultimate URLs for Zen Cart.
  *
- * @copyright Copyright 2019-2021 Cindy Merkin (vinosdefrutastropicales.com)
+ * @copyright Copyright 2019-2022 Cindy Merkin (vinosdefrutastropicales.com)
  * @license http://www.gnu.org/licenses/gpl.txt GNU GPL V3.0
  */
 if (!defined('IS_ADMIN_FLAG') || IS_ADMIN_FLAG !== true) {
     die('Illegal Access');
 }
 
-class UsuAdminObserver extends base 
+class UsuAdminObserver extends base
 {
-    public function __construct() 
+    public function __construct()
     {
-        $this->enabled = (defined('USU_ENABLED') && USU_ENABLED == 'true');
+        $this->enabled = (defined('USU_ENABLED') && USU_ENABLED === 'true');
         if ($this->enabled) {
-            $this->attach (
+            $this->attach(
                 $this, 
-                array( 
+                [
                     /* From /admin/includes/functions/html_output.php */
-                   'NOTIFY_SEFU_INTERCEPT_ADMCATHREF', 
-                )
+                   'NOTIFY_SEFU_INTERCEPT_ADMCATHREF',
+                ]
             );
         }
     }
   
-    public function update(&$class, $eventID, $p1, &$p2, &$p3, &$p4, &$p5, &$p6, &$p7, &$p8) 
+    public function update(&$class, $eventID, $p1, &$p2, &$p3, &$p4, &$p5, &$p6, &$p7, &$p8)
     {
         // -----
         // Moved here (was in constructor) so that any action detected by USU's admin initialization
@@ -43,7 +43,7 @@ class UsuAdminObserver extends base
         switch ($eventID) {
             // -----
             // Issued at the top of the zen_catalog_href_link function to allow us to override the href-link
-            // returned.
+            // returned.  The link generated indicates that the admin's session-id not be included!
             //
             // On entry:
             //
@@ -54,7 +54,7 @@ class UsuAdminObserver extends base
             // $p5 ... A reference to the (input) connection type (either 'SSL' or 'NONSSL') to be used for the link
             //
             case 'NOTIFY_SEFU_INTERCEPT_ADMCATHREF':
-                $p2 = $this->usu->href_link($p3, $p4, $p5);
+                $p2 = $this->usu->href_link($p3, $p4, $p5, false);
                 break;
 
             default:
