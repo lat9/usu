f777540c6310a929bc8504c6d4756ca1e1123623
lat9 <cindy@vinosdefrutastropicales.com>
2026-05-15 09:39:06 -0400

    "Really" convert to array-based language files
    Ref #67

--- YOUR_ADMIN/includes/init_includes/init_usu_admin_install.php
+++ YOUR_ADMIN/includes/init_includes/init_usu_admin_install.php
@@ -3,7 +3,9 @@
  * Part of Ultimate URLs for Zen Cart, v3.1.0+. This script is loaded by admin/init_includes/init_usu_admin.php 
  * on an initial installation of the plugin or an upgrade from a version prior to v3.0.1.
  *
- * @copyright Copyright 2019, 2023 Cindy Merkin (vinosdefrutastropicales.com)
+ * Last updated: v4.0.1
+ *
+ * @copyright Copyright 2019, 2026 Cindy Merkin (vinosdefrutastropicales.com)
  * @license http://www.gnu.org/licenses/gpl.txt GNU GPL V3.0
  */
 if (!defined('IS_ADMIN_FLAG')) {
@@ -100,10 +102,48 @@ if (zen_page_key_exists('configUltimateSEO')) {
 }
 
 // -----
-// The language-text associated with each of the above options is present in a language file
-// that is loaded only upon the plugin's initial installation.
+// The language-text associated with each of the above options is present in this array.
 //
-require DIR_WS_LANGUAGES . 'english/usu_configuration.php';
+$titles_descriptions = [
+    'USU_ENABLED_TITLE' => 'Enable alternate URLs?',
+    'USU_ENABLED_DESCRIPTION' => 'This is a global setting that enables (<b>true</b>) or disables (<b>false</b>) the alternate URLs\' generation.',
+
+    'USU_DEBUG_TITLE' => 'Enable debug log?',
+    'USU_DEBUG_DESCRIPTION' => 'When enabled, additional debugging information is saved to log files (<code>/logs/usu-{adm-}yyyymmmdd-hhmmss.log</code>.<br><br>Enabling debugging may result in the creation of numerous log files and may adversely affect server performance. Only enable this if absolutely necessary!',
+
+    'USU_CPATH_TITLE' => 'Generate cPath parameters',
+    'USU_CPATH_DESCRIPTION' => 'By default Zen Cart generates a cPath parameter for product pages. These are used to keep linked products in the correct category. In automatic mode the cPath will only be added if needed.',
+
+    'USU_END_TITLE' => 'Alternate URLs end with',
+    'USU_END_DESCRIPTION' => 'If you want your URLs to end with a certain suffix add one here. Common suffixes are \'.html\', \'.htm\'. Leave this field blank to have no suffix added to generated URLs.',
+
+    'USU_FORMAT_TITLE' => 'Format of alternate URLs',
+    'USU_FORMAT_DESCRIPTION' => 'You can select from a list of commonly generated formats.<br><br><b>Original:</b><ul><li><i>Categories:</i> category-name-c-34</li><li><i>Products:</i> product-name-p-54</li></ul><b>Category Parent:</b><ul><li><i>Categories:</i> parent-category-name-c-34</li><li><i>Products:</i> parent-product-name-p-54</li></ul>',
+
+    'USU_CATEGORY_DIR_TITLE' => 'Display categories as directories',
+    'USU_CATEGORY_DIR_DESCRIPTION' => 'You can select from a list of commonly generated formats.<br><b>Off:</b> disables displaying categories as directories<br><br><b>Short:</b> use the settings from \'Format of alternate URLs\'<br><br><b>Full:</b> uses full category paths<br><br>',
+
+    'USU_REMOVE_CHARS_TITLE' => 'Remove problematic characters',
+    'USU_REMOVE_CHARS_DESCRIPTION' => 'This allows you remove certain problematic characters from the generated URLs.<br><br><i>non-alphanumerical:</i> removes all non-alphanumerical characters<br><i>punctuation:</i> removes all punctuation characters',
+
+    'USU_FILTER_PCRE_TITLE' => 'Enter PCRE filter rules',
+    'USU_FILTER_PCRE_DESCRIPTION' => 'This setting uses PCRE rules to filter URLs.<br><br>This filter is run before character conversions and stripping of special characters. If you want a dash - in your URLS, use a single space. To escape a character in the regular expression use \\\\ instead of a single \\.<br><br>The format <b>MUST</b> be in the form: <b>find1=>replace1,find2=>replace2</b>. ',
+
+    'USU_FILTER_SHORT_WORDS_TITLE' => 'Filter short words',
+    'USU_FILTER_SHORT_WORDS_DESCRIPTION' => 'This setting will filter &quot;short&quot; words, i.e. those with length less than or equal to the value specified, from any generated URLs.  Use the value <b>0</b> to include <em>all</em> words.',
+
+    'USU_FILTER_PAGES_TITLE' => 'Limit alternate URLS to the following pages',
+    'USU_FILTER_PAGES_DESCRIPTION' => 'You can limit the pages which will be rewritten by specifying them here. If no pages are specified all pages will be rewritten.<br><br>The format is a comma-delimited list (intervening spaces are OK) and <b>must</b> be in the form: <b>page1,page2,page3</b> or <b>page1, page2, page3</b>',
+
+    'USU_ENGINE_TITLE' => 'Choose URL Engine',
+    'USU_ENGINE_DESCRIPTION' => 'Choose which URL Engine to use.',
+
+    'USU_REDIRECT_TITLE' => 'Enable automatic redirects?',
+    'USU_REDIRECT_DESCRIPTION' => 'This will activate the automatic redirect code and send 301 headers for old to new URLs.',
+
+    'USU_VERSION_TITLE' => 'Plugin Version',
+    'USU_VERSION_DESCRIPTION' => 'The currently-installed version of <em>USU</em>.',
+];
 
 // -----
 // Loop, creating each of the default configuration values, after setting some common
@@ -134,8 +174,8 @@ foreach ($usu_install_config as $key => $data) {
     $key = "USU_$key";
     $key_data = [
         'configuration_key' => $key,
-        'configuration_title' => constant($key . '_TITLE'),
-        'configuration_description' => constant($key . '_DESCRIPTION')
+        'configuration_title' => $titles_descriptions[$key . '_TITLE'],
+        'configuration_description' => $titles_descriptions[$key . '_DESCRIPTION'],
     ];
     $usu_config = array_merge($default_data, $data, $key_data);
     $default_data['sort_order'] += 10;
@@ -145,5 +185,5 @@ foreach ($usu_install_config as $key => $data) {
 // -----
 // Record the plugin's configuration-access in the admin pages.
 //
-zen_register_admin_page('configUltimateSEO', 'BOX_CONFIGURATION_USU', 'FILENAME_CONFIGURATION', "gID=$cgi", 'configuration', 'Y', $cgi);
+zen_register_admin_page('configUltimateSEO', 'BOX_CONFIGURATION_USU', 'FILENAME_CONFIGURATION', "gID=$cgi", 'configuration', 'Y');
 
